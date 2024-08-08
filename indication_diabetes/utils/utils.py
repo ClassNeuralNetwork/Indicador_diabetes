855063a4a0dd36daa91ad6de73688d41ba88e143
roscibely <rosanacibely@gmail.com>
2024-08-08 17:33:15 -0300

    refactor: refactored database

--- indication_diabetes/utils/utils.py
+++ indication_diabetes/utils/utils.py
@@ -3,14 +3,14 @@ from sklearn.model_selection import train_test_split
 
 db = pd.read_csv("/home/brunopaiva/DataSet/indication_diabetes/utils/diabetes_012_health_indicators_BRFSS2021.csv")
 
-db['Diabetes_012'] = db['Diabetes_012'].astype('category').cat.codes
+y = db['Diabetes_binary'] # Classes/Labels
+df = db.drop(['Diabetes_binary'], axis = 1)
+x = db.iloc[:, 0:22].values # Atributos/Features
 
-db.head()
-db.info()
-classes = db['Diabetes_012'].value_counts()
+classes = db['Diabetes_binary'].value_counts()
 print(classes)
 
-input_train, input_test, output_train, output_test = train_test_split(db.iloc[:, 0:4].values, db['Diabetes_012'].values, test_size=0.2)
+input_train, input_test, output_train, output_test = train_test_split(x, y, test_size=0.2)
 
 input_train = pd.DataFrame(input_train)
 input_train.to_csv('input_train_csv', index=False)
