3407ace88e8c959c3871ed349997864fa88e0c4c
brunopaiva1 <brunopaivamaster10@gmail.com>
2024-08-09 14:34:42 -0300

    normalizing data and creating standard data in mlp.py

--- indication_diabetes/model/mlp.py
+++ indication_diabetes/model/mlp.py
@@ -7,9 +7,28 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.callbacks import EarlyStopping
 
-x_treino_padrao = pd.read_csv('/home/brunopaiva/DataSet/Indicador_diabetes/indication_diabetes/dataset/train/input_train_balanced.csv')
-saida_treino = pd.read_csv('/home/brunopaiva/DataSet/Indicador_diabetes/indication_diabetes/dataset/train/output_train_balanced.csv')
-x_treino_padrao.info()
+# x_treino_padrao = pd.read_csv('/home/brunopaiva/DataSet/Indicador_diabetes/indication_diabetes/dataset/train/input_train_balanced.csv')
+# saida_treino = pd.read_csv('/home/brunopaiva/DataSet/Indicador_diabetes/indication_diabetes/dataset/train/output_train_balanced.csv')
+# x_treino_padrao.info()
+
+#Carregar os dados
+input_train = pd.read_csv('/home/brunopaiva/DataSet/Indicador_diabetes/indication_diabetes/dataset/train/input_train.csv')
+output_train = pd.read_csv('/home/brunopaiva/DataSet/Indicador_diabetes/indication_diabetes/dataset/train/output_train.csv')
+input_test = pd.read_csv('/home/brunopaiva/DataSet/Indicador_diabetes/indication_diabetes/dataset/test/input_test.csv')
+output_test = pd.read_csv('/home/brunopaiva/DataSet/Indicador_diabetes/indication_diabetes/dataset/test/output_test.csv')
+
+#Normalizando os dados
+scaler = StandardScaler()
+input_train_scaled = scaler.fit_transform(input_train)
+input_test_scaled = scaler.transform(input_test)
+
+#DataFrame com os nomes das colunas
+#input_train_standard = pd.DataFrame(input_train_scaled)
+
+#Salvando os dados normalizados
+pd.DataFrame(input_train_scaled).to_csv('/home/brunopaiva/DataSet/Indicador_diabetes/indication_diabetes/dataset/train/input_train_standard.csv')
+pd.DataFrame(input_test_scaled).to_csv('/home/brunopaiva/DataSet/Indicador_diabetes/indication_diabetes/dataset/test/input_test_standard.csv')
+
 
 # e camada de saída com 2 neurônios (2 classes)
 early_stopping = EarlyStopping(monitor='val_loss', patience=10, mode='min', restore_best_weights=True)
@@ -17,10 +36,10 @@ model = tf.keras.models.Sequential()
 # model.add(tf.keras.layers.Dense(500, input_dim=21, kernel_regularizer=tf.keras.regularizers.L2(0.01), activation='sigmoid', name='oculta'))
 # model.add(tf.keras.layers.Dropout(0.2))
 model.add(tf.keras.layers.Dense(256, activation='relu', input_shape=(21,)))
-
+model.add(tf.keras.layers.Dropout(0.1))
 # Camada oculta 2 com 400 neurônios
 model.add(tf.keras.layers.Dense(128, activation='relu'))
-
+model.add(tf.keras.layers.Dropout(0.1))
 # Camada oculta 3 com 300 neurônios
 model.add(tf.keras.layers.Dense(64, activation='relu'))
 
@@ -40,7 +59,7 @@ opt = tf.keras.optimizers.Adam(learning_rate=0.01)
 model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
 
 # Train model
-history = model.fit(x_treino_padrao, saida_treino, validation_split=0.2, epochs=50, callbacks=[early_stopping])
+history = model.fit(input_train_scaled, output_train, validation_split=0.2, epochs=50, callbacks=[early_stopping])
 
 
 model.save('model.keras')
