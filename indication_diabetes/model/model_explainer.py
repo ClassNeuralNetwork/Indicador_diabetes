32594947041ec1fdc1ebbd37995fd9255c5efaeb
brunopaiva1 <brunopaivamaster10@gmail.com>
2024-08-15 23:32:55 -0300

    removendo importações

--- indication_diabetes/model/model_explainer.py
+++ indication_diabetes/model/model_explainer.py
@@ -1,10 +1,6 @@
 import tensorflow as tf
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import LabelEncoder
 import shap
 
 # Carregar o modelo
